bfe90595267e3cc87694e28f3a737d9b8a23bd14
unknown <javascript.js.py@gmail.com>
2021-11-04 23:13:47 +0600

    fdf

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,12 @@ export class AuthController {
   @Get('')
   getAll(@Res() res, @Body() body){
     const vCard = vCardJS();
-    vCard.firstName = body.name;
+    vCard.firstName = "Lukman";
     vCard.middleName = 'Full Stack Developer';
-    vCard.lastName = body.surname;
+    vCard.lastName ="Second Name";
     vCard.organization = 'Motionweb Corporation';
     vCard.photo.attachFromUrl('https://avatars2.githubusercontent.com/u/5659221?v=3&s=460', 'JPEG');
-    vCard.workPhone = '312-555-1212';
+    vCard.workPhone = '0706048466';
     vCard.birthday = new Date(1985, 0, 1);
     vCard.title = 'Full Stack Developer';
     vCard.url = 'https://github.com/lukman-wp';
